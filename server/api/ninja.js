b6919a1843e117c6b07e7d15801947c3bcc03601
saugat <rai@hbss.co.jp>
2023-07-03 11:34:17 +0900

    public api key removed from projeect

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -2,6 +2,9 @@
 export default defineEventHandler(async (event) => {
     // const { name } = getQuery(event);
     // const { age } = await readBody(event);
-    const { data } = await $fetch('https://api.currencyapi.com/v3/latest?api_key=eJdPhICJUDBt6B4e4IX6DZ4A9Xos4Z2JRZ8xh2v2');
-    return data;
+
+    const { currencyKey } = useRuntimeConfig();
+    const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&api_key=${currencyKey}`
+    const { data } = await $fetch(uri)
+    return data
 });
